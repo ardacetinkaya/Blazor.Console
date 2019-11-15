70858914328dcf4af23213c67927e541f87bf8a2
Arda Cetinkaya <ardacetinkaya@gmail.com>
2019-11-15 11:42:41 +0300

    Update BlazorConsole.razor.cs

--- src/Blazor.Console/BlazorConsole.razor.cs
+++ src/Blazor.Console/BlazorConsole.razor.cs
@@ -7,17 +7,14 @@ namespace Blazor.Console
     public class BlazorConsoleComponent : ComponentBase
     {
         protected string terminal = string.Empty;
-        protected string input = string.Empty;
+
 
         protected TerminalCommand command = new TerminalCommand();
-        protected class TerminalCommand
-        {
-            public string Text { get; set; }
-        }
+
         protected async Task Execute(EditContext context)
         {
             var cmd = context.Model as TerminalCommand;
-            terminal += $"<p><span style='color:white;font-weight:bold'>{DateTime.Now.ToShortTimeString()} > </span><span style='color:yellow'>{cmd.Text}{Environment.NewLine}</span>";
+            terminal += $"<p><span style='color:white;font-weight:bold'>{cmd.Time.ToShortTimeString()} > </span><span style='color:yellow'>{cmd.Text}{Environment.NewLine}</span>";
             terminal += $"{Output(cmd.Text)}";
             terminal += $"</p>";
             command.Text = string.Empty;
@@ -29,11 +26,21 @@ namespace Blazor.Console
             string output = string.Empty;
             switch (text)
             {
+                case "os":
+                    output += $"<p>";
+                    output += $"<span style='color:white'>{System.Runtime.InteropServices.RuntimeInformation.OSDescription}</span>";
+                    output += $"</p>";
+                    break;
+                case "version":
+                    output += $"<p>";
+                    output += $"<span style='color:white'>{System.Reflection.Assembly.GetExecutingAssembly()}</span>";
+                    output += $"</p>";
+                    break;
                 case "help":
                     output += $"<p>";
-                    output += $"<span style='color:white'>{string.Format("{0,10}", "help")}</span>";
-                    output += $"<span style='color:white'>{string.Format("{0,10}", "help")}</span>";
-                    output += $"<span style='color:white'>{string.Format("{0,10}", "help")}</span>";
+                    output += $"<span style='color:white;display:block'>{string.Format("help\t\tDisplays commands that are available")}</span>";
+                    output += $"<span style='color:white;display:block'>{string.Format("version\t\tDisplays commands that are available")}</span>";
+                    output += $"<span style='color:white;display:block'>{string.Format("os\t\tDisplays commands that are available")}</span>";
                     output += $"</p>";
                     break;
                 default:
@@ -46,5 +53,10 @@ namespace Blazor.Console
             return output;
         }
 
+    }
+    public class TerminalCommand
+    {
+        public string Text { get; set; }
+        public DateTime Time { get; } = DateTime.Now;
     }
 }
