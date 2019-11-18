2d76c9510671176d4c66f2b083ba3230d7dd93ca
Arda Cetinkaya <ardacetinkaya@gmail.com>
2019-11-18 16:33:36 +0300

    Update CommandInput.cs

--- src/Blazor.Console/Command/CommandInput.cs
+++ src/Blazor.Console/Command/CommandInput.cs
@@ -1,39 +1,58 @@
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
-
 namespace Blazor.Console.Command
 {
+    using Microsoft.Extensions.DependencyInjection;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Logging;
+    using CommandLine;
+    
     public class Input
     {
         public string Text { get; set; }
         public DateTime Time { get; } = DateTime.Now;
     }
 
-    public class CommandInput
+    internal class CommandInput
     {
         public string Text { get; set; }
         public DateTime Time { get; } = DateTime.Now;
 
+        public Dictionary<string, ICommand> Commands
+        {
+            get
+            {
+                return _commands;
+            }
+        }
+
         readonly ICommandRunning _runningCommand;
-        readonly IEnumerable<string> _commands;
+        readonly Dictionary<string, ICommand> _commands;
         readonly ILogger<CommandInput> _logger;
-        IServiceProvider _provider;
+        readonly IServiceProvider _provider;
 
         public CommandInput(ILogger<CommandInput> logger, IServiceProvider provider, ICommandRunning runningCommand)
         {
             _provider = provider;
             _runningCommand = runningCommand;
             _logger = logger;
-            _commands = new List<string>()
+
+            _commands = new Dictionary<string, ICommand>();
+
+        }
+
+        public Dictionary<string, ICommand> AddCommands(Dictionary<string, ICommand> commands)
+        {
+            foreach (var command in commands)
             {
-                {"os"}
-                ,{"help"}
-                ,{"version"}
-            };
+                if (!Commands.ContainsKey(command.Key))
+                {
+                    Commands.Add(command.Key, command.Value);
+                }
+            }
+
+            return Commands;
         }
 
         public async Task<string> Result()
@@ -41,26 +60,22 @@ namespace Blazor.Console.Command
             string output = string.Empty;
             ICommand command = null;
             string[] arguments = null;
+
             try
             {
-
-                if (Text.Trim().IndexOf(" ") == -1)
-                {
-
-                }
-                else
-                {
-                    arguments = Text.Split(' ',2,StringSplitOptions.RemoveEmptyEntries);
-                }
-
                 command = (Text switch
                 {
                     "lng" => _ = _provider.GetRequiredService<ILongRunningCommand>(),
                     "os" => _ = _provider.GetRequiredService<IOSCommand>(),
                     "version" => _ = _provider.GetRequiredService<IVersionCommand>(),
                     "help" => _ = _provider.GetRequiredService<IHelpCommand>(),
-                    _ => _ = new InvalidCommand(Text)
+                    _ => _ = _commands[Text]
                 });
+
+                if(command is IHelpCommand)
+                {
+                    (command as IHelpCommand).Commands = Commands;
+                }
             }
             catch (System.Exception)
             {
